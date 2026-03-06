13a09f076cd27cf50e17f850658508ce5e7c8356
Stackie Jia <jsq2627@gmail.com>
2026-03-07 06:09:01 +0800

    fix: remove copyright notice from usage output

--- src/configuration.c
+++ src/configuration.c
@@ -955,7 +955,6 @@ void usage(FILE *f, char *progname) {
       " - Multicast RTP to Unicast HTTP stream convertor\n"
       "\n"
       "Version " VERSION "\n"
-      "Copyright 2008-2025 Ondrej Caletka <ondrej@caletka.cz>\n"
       "\n"
       "This program is free software; you can redistribute it and/or modify\n"
       "it under the terms of the GNU General Public License version 2\n"
